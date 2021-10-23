ae9c1410454fcb04ba60acea23f0a9165f19ed7a
Roy Biesheuvel <roy_biesheuvel@hotmail.com>
2021-10-23 14:50:17 +0200

    Started working on direction based skull movement

--- SkullGame/skull.cpp
+++ SkullGame/skull.cpp
@@ -11,29 +11,53 @@ public:
 
 	Vector2 Position = { (float)GetRandomValue(0, GetScreenWidth()), 0.0f };
 
-	Vector2 RandomDeviation = { (float)GetRandomValue(-3, 3), (float)GetRandomValue(-3, 3) };
-
 	float Speed = (float)GetRandomValue(80, 150);
 	float SpeedIncrement = (float)GetRandomValue(15, 30);
 	float SpeedLimit = (float)GetRandomValue(500, 800);
 	
+	float RotationSpeed;
+	float RotationSpeedMax = (float)GetRandomValue(5, 10);
+	float RotationOscillation = (float)GetRandomValue(1, 4);
 
+	float LocalTime = 0;
+	float LocalTimeMax = 1;
+	
+	Vector2 Direction = { 0, 1 };
 
 	explicit Skull(Vector2* targetPosition, Texture2D* skullTexture) 
 		: TargetPosition(targetPosition)
 		, SkullTexture(skullTexture) { }
 
 	void Update() override {
-		Position = Vector2Add(Position, Vector2Multiply(RandomDeviation, { GetFrameTime(), GetFrameTime() }));
+		// Local time
+		if (LocalTime < LocalTimeMax) {
+			LocalTime += GetFrameTime();
+		}
+		else {
+			LocalTime = 0;
+		}
 
+		// Slowly increase speed
 		if (Speed < SpeedLimit) {
 			Speed += SpeedIncrement * GetFrameTime();
 		}
 
-		Position = Vector2MoveTowards(Position, *TargetPosition, Speed * GetFrameTime());
+		// Oscilate the rotationSpeed
+		RotationSpeed = RotationSpeedMax - (RotationOscillation * (LocalTime / LocalTimeMax));
+
+
+		Position = Vector2Add(Position, Direction);
+
+		Direction = Vector2Rotate(Direction, RotationSpeed * GetFrameTime());
+		
+		// Old way of moving directly
+		// Position = Vector2MoveTowards(Position, *TargetPosition, Speed * GetFrameTime());
 	}
 
 	void Draw() override {
 		DrawTextureEx(*SkullTexture, Position, 0, 4, WHITE);
+
+		DrawRectangle(20, 420, 1000, 20, BLACK);
+		DrawText(TextFormat("doei %f", LocalTime / LocalTimeMax), 20, 420, 20, WHITE);
 	}
 };
