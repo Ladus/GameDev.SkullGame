851ca90e52e79afe3623f86fc17ba72fb6a2757d
Roy Biesheuvel <roy_biesheuvel@hotmail.com>
2021-10-23 13:17:17 +0200

    Implemented GameObject interface and moved logic to skull

--- SkullGame/skull.cpp
+++ SkullGame/skull.cpp
@@ -1,12 +1,34 @@
 #include "raylib.h"
 #include "raymath.h"
 
-class Skull {
-	public:
-		Vector2 Position = { (float)GetRandomValue(0, GetScreenWidth()), 0.0f };
-		float Speed = (float) GetRandomValue(80,150);
-		float SpeedIncrement = (float)GetRandomValue(15,30);
-		float SpeedLimit = (float)GetRandomValue(500,800);
-		Texture2D Texture = LoadTexture("Assets/skull_stage.png");
-		Vector2 RandomDeviation = { (float) GetRandomValue(-3, 3), (float)GetRandomValue(-3, 3) };
+#include "GameObject.cpp"
+
+class Skull : public GameObject {
+public:
+
+	Vector2* TargetPosition;
+
+	Vector2 Position = { (float)GetRandomValue(0, GetScreenWidth()), 0.0f };
+	float Speed = (float)GetRandomValue(80, 150);
+	float SpeedIncrement = (float)GetRandomValue(15, 30);
+	float SpeedLimit = (float)GetRandomValue(500, 800);
+	Texture2D Texture = LoadTexture("Assets/skull_stage.png");
+	Vector2 RandomDeviation = { (float)GetRandomValue(-3, 3), (float)GetRandomValue(-3, 3) };
+
+
+	explicit Skull(Vector2* targetPosition) : TargetPosition(targetPosition) { }
+
+	void Update() override {
+		Position = Vector2Add(Position, Vector2Multiply(RandomDeviation, { GetFrameTime(), GetFrameTime() }));
+
+		if (Speed < SpeedLimit) {
+			Speed += SpeedIncrement * GetFrameTime();
+		}
+
+		Position = Vector2MoveTowards(Position, *TargetPosition, Speed * GetFrameTime());
+	}
+
+	void Draw() override {
+		DrawTextureEx(Texture, Position, 0, 4, WHITE);
+	}
 };
